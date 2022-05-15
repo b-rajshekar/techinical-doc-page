bbfc08d2432bd9f29f07fcedb862ec81aa4132da
b-rajshekar <brajashekhar19@gmail.com>
2022-05-15 06:25:22 +0530

    Added JS File

---
+++ script.js
@@ -0,0 +1,8 @@
+// For smooth scrolling
+$('a[href^="#"]').click(function () {
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+
+    return false;
+});

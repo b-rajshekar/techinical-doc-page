1eebbd1ef32a34ce463f3bceb47142db1931f896
b-rajshekar <brajashekhar19@gmail.com>
2022-05-15 06:35:00 +0530

    Removed js file

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-// For smooth scrolling
-$('a[href^="#"]').click(function () {
-    $('html, body').animate({
-        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-    }, 500);
-
-    return false;
-});
+// window.scroll({
+//     top: 2500, 
+//     left: 0, 
+//     behavior: 'smooth'
+//   });
+  
+//   // Scroll certain amounts from current position 
+//   window.scrollBy({ 
+//     top: 100, // could be negative value
+//     left: 0, 
+//     behavior: 'smooth' 
+//   });
+  
+//   // Scroll to a certain element
+//   document.querySelector('.hello').scrollIntoView({ 
+//     behavior: 'smooth' 
+//   });
